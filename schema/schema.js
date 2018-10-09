7e2290c5e465255acfdc719ab7136bb10de2532f
akashiintellect <akash.iintellect@gmail.com>
2018-10-09 17:52:29 +0500

    sql-graphql-with-relation

--- schema/schema.js
+++ schema/schema.js
@@ -31,13 +31,24 @@ connection.connect(function(err) {
     console.log('connected as id ' + connection.threadId);
 });
 var users=[];
-
-const connectDb = new Promise((resolve,reject)=>{
+var certs=[];
+const getUsers = new Promise((resolve,reject)=>{
     connection.query('SELECT * FROM name', function (error, results, fields) {
         if (error) {
             console.log(error);
         }
         console.log('users length: ', results.length);
+        resolve(results);
+    });
+});
+
+
+const getCerts = new Promise((resolve,reject)=>{
+    connection.query('SELECT * FROM cert_register', function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        }
+        console.log('certss length: ', results.length);
         resolve(results);
     });
 })
@@ -50,8 +61,10 @@ const disconnectDb=()=>{
 
 
 async function connectAndDesconnectAW() {
-    users = await connectDb;
-    console.log("fetch completed");
+    users = await getUsers;
+    console.log("Users fetch completed");
+    certs = await getCerts;
+    console.log("Certs fetch completed");
     const disConnectResult= await disconnectDb();
     console.log(disConnectResult);
 
@@ -76,6 +89,38 @@ const CompanyType = new GraphQLObjectType({
     }
 });
 
+//
+//
+//
+
+const UserCertType = new GraphQLObjectType({
+    name: 'UserCert',
+    fields:() => ( {
+        Id: { type: GraphQLInt },
+        FIRST_NAME: { type: GraphQLString },
+        CHAPTER: { type: GraphQLString },
+        cert_registers: {
+            type: new GraphQLList(CertUserType),
+            resolve(parent, args){
+                return _.filter(certs, {STUDENT_ID: parent.Id})
+            }
+        }
+    })});
+const CertUserType = new GraphQLObjectType({
+        name: 'CertUser',
+        fields: () => ({
+            SEQN: {type: GraphQLInt},
+            STUDENT_ID: {type: GraphQLInt},
+            user: {
+                type: UserCertType,
+                resolve(parent, args){
+                    return _.find(users, {Id: parent.STUDENT_ID})
+                }
+            }
+        })
+    }
+)
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -98,10 +143,25 @@ const RootQuery = new GraphQLObjectType({
             resolve( parentValue, args ) {
                 return _.find( users, { CO_ID: args.companyId } )
             }
+        },
+        userCert: {
+            type: UserCertType,
+            args: { id: { type: GraphQLInt } },
+            resolve( parentValue, args ) {
+                return _.find(users, { Id: args.id });
+            }
+        },
+        certUser: {
+            type: CertUserType,
+            args: {SEQN: {type: GraphQLInt}},
+            resolve(parent, args){
+                return _.find(certs, {SEQN: args.SEQN})
+            }
         }
     }
-});
 
+
+});
 
 module.exports = new GraphQLSchema({
     query: RootQuery
