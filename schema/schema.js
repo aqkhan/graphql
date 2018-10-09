5648ceb66f11c0d01068b509e9704f1be78174a0
akashiintellect <akash.iintellect@gmail.com>
2018-10-09 17:41:44 +0500

    with-relation-complete-json

--- schema/schema.js
+++ schema/schema.js
@@ -11,7 +11,11 @@ const {
 // To walk through data
 const _ = require('lodash');
 
+
 let users = require('./test-data/name_full');
+console.log(`users length: ${users.length}`);
+let certs = require('./test-data/cert_register');
+console.log(`certifications length: ${certs.length}`)
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -30,6 +34,38 @@ const CompanyType = new GraphQLObjectType({
         Id: { type: GraphQLString },
     }
 });
+
+//
+//
+//
+
+const UserCertType = new GraphQLObjectType({
+    name: 'UserCert',
+    fields:() => ( {
+        Id: { type: GraphQLInt },
+        FIRST_NAME: { type: GraphQLString },
+        CHAPTER: { type: GraphQLString },
+        cert_registers: {
+            type: new GraphQLList(CertUserType),
+            resolve(parent, args){
+                return _.filter(certs, {STUDENT_ID: parent.Id})
+            }
+        }
+    })});
+const CertUserType = new GraphQLObjectType({
+        name: 'CertUser',
+        fields: () => ({
+            SEQN: {type: GraphQLInt},
+            STUDENT_ID: {type: GraphQLInt},
+            user: {
+                type: UserCertType,
+                resolve(parent, args){
+                    return _.find(users, {Id: parent.STUDENT_ID})
+                }
+            }
+        })
+    }
+)
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
@@ -53,8 +89,24 @@ const RootQuery = new GraphQLObjectType({
             resolve( parentValue, args ) {
                 return _.find( users, { CO_ID: args.companyId } )
             }
+        },
+        userCert: {
+            type: UserCertType,
+            args: { id: { type: GraphQLInt } },
+            resolve( parentValue, args ) {
+                return _.find(users, { Id: args.id });
+            }
+        },
+        certUser: {
+            type: CertUserType,
+            args: {SEQN: {type: GraphQLInt}},
+            resolve(parent, args){
+                return _.find(certs, {SEQN: args.SEQN})
+            }
+            }
         }
-    }
+
+
 });
 
 module.exports = new GraphQLSchema({
